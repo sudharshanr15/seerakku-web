57d32b84eff5b92763fabcdf6a53e10d70677eec
Sudharshan <sudharshanrms2004@gmail.com>
2023-07-20 17:01:17 +0530

    Disabled background scroll on overlay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="data-[has-overflow=true]:overflow-hidden">
         <NavBar />
         {children}
         <Footer />
